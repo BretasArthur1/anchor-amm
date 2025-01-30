c7acbadebdce7b3c340b923f473808b8735f28fc
BretasArthur1 <arthur.bretas@sou.inteli.edu.br>
2025-01-30 20:41:26 -0300

    anchor amm program

---
+++ programs/amn-anchor/src/lib.rs
@@ -0,0 +1,30 @@
+use anchor_lang::prelude::*;
+
+mod errors;
+mod state;
+mod instructions;
+
+use instructions::*;
+
+declare_id!("4TZK6jJ3kM94RvgUEno2bqVr4QoJh52cdxVkzDZANGnH");
+
+#[program]
+pub mod amm_anchor {
+    use super::*;
+
+    pub fn init(ctx: Context<Init>, seed: u64, fee: u16, authority: Option<Pubkey>) -> Result<()> {
+        ctx.accounts.init(seed, fee, authority, ctx.bumps)
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+        ctx.accounts.deposit(amount, max_x, max_y)
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+        ctx.accounts.withdraw(amount, max_x, max_y)
+    }
+
+    pub fn swap(ctx: Context<Swap>, is_x: bool, amount_in: u64, min_amount_out: u64) -> Result<()> {
+        ctx.accounts.swap(is_x, amount_in, min_amount_out)
+    }
+}
